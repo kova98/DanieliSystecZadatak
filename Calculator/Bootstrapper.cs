b1602b4925c2536a09b69cde7318c84ca5e72c6b
Roko <rokokovac98@gmail.com>
2018-12-11 19:08:25 +0100

    Set up Caliburn.Micro

---
+++ Calculator/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Calculator.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Calculator
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
